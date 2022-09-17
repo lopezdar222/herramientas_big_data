27f35943d83e63a03a8eb678c8708d087c5a405f
lopezdar222 <41219954+lopezdar222@users.noreply.github.com>
2022-09-17 18:55:16 -0300

    v4

---
+++ Paso00.sh
@@ -0,0 +1,11 @@
+sudo docker cp Datasets/canaldeventa/CanalDeVenta.csv namenode:/home/Datasets/canaldeventa/CanalDeVenta.csv
+sudo docker cp Datasets/calendario/Calendario.csv namenode:/home/Datasets/calendario/Calendario.csv
+sudo docker cp Datasets/cliente/Cliente.csv namenode:/home/Datasets/cliente/Cliente.csv
+sudo docker cp Datasets/compra/Compra.csv namenode:/home/Datasets/compra/Compra.csv
+sudo docker cp Datasets/empleado/Empleado.csv namenode:/home/Datasets/empleado/Empleado.csv
+sudo docker cp Datasets/gasto/Gasto.csv namenode:/home/Datasets/gasto/Gasto.csv
+sudo docker cp Datasets/producto/Producto.csv namenode:/home/Datasets/producto/Producto.csv
+sudo docker cp Datasets/proveedor/Proveedor.csv namenode:/home/Datasets/proveedor/Proveedor.csv
+sudo docker cp Datasets/sucursal/Sucursal.csv namenode:/home/Datasets/sucursal/Sucursal.csv
+sudo docker cp Datasets/tipodegasto/TiposDeGasto.csv namenode:/home/Datasets/tipodegasto/TiposDeGasto.csv
+sudo docker cp Datasets/venta/Venta.csv namenode:/home/Datasets/venta/Venta.csv
