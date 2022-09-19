66c59b35f29d410ab8b69d71c1bd4cb8285434ac
lopezdar222 <41219954+lopezdar222@users.noreply.github.com>
2022-09-19 19:35:24 -0300

    spark

--- Paso05.py
+++ Paso05.py
@@ -14,13 +14,13 @@ spark = SparkSession.\
         config("spark.executor.memory", "512m").\
         getOrCreate()
 
-venta = spark.read.option("compression.codec", "snappy").option("mergeSchema", "true").parquet("data2/venta")
+venta = spark.read.option("compression.codec", "snappy").option("mergeSchema", "true").parquet("hdfs://namenode:9000/data2/venta")
 
 ventas_out = venta.na.drop(subset=['precio','cantidad']).groupBy("idproducto").agg(mean(venta.precio).alias("promedio"), stddev(venta.precio).alias("stddev"))
 
 ventas_out = ventas_out.withColumn("PrecioMaximo", ventas_out.promedio + ventas_out.stddev * 3).withColumn("PrecioMinimo", ventas_out.promedio - ventas_out.stddev * 3)
 
-ventas_out.write.option("compression.codec", "snappy").option("mergeSchema", "true").parquet("data2/venta_criterio_outliers", mode="overwrite")
+ventas_out.write.option("compression.codec", "snappy").option("mergeSchema", "true").parquet("hdfs://namenode:9000/data2/venta_criterio_outliers", mode="overwrite")
 
 venta = venta.alias("v").join(ventas_out.alias("o"), venta['idproducto'] == ventas_out['idproducto']).select("v.idventa","v.fecha","v.fecha_entrega","v.idcanal","v.idcliente","v.idsucursal","v.idempleado","v.idproducto","v.precio","v.cantidad","o.promedio","o.stddev","o.PrecioMaximo","o.PrecioMinimo")
 
@@ -37,7 +37,7 @@ venta = venta.withColumn("esOutlier", udf_detecta_outlier(venta.precio, venta.Pr
 
 venta = venta.select(["idventa","fecha","fecha_entrega","idcanal","idcliente","idsucursal","idempleado","idproducto","precio","cantidad"])
 
-venta.write.option("compression.codec", "snappy").option("mergeSchema", "true").parquet("data2/venta_sin_outliers", mode="overwrite")
+venta.write.option("compression.codec", "snappy").option("mergeSchema", "true").parquet("hdfs://namenode:9000/data2/venta_sin_outliers", mode="overwrite")
 
 endTimeQuery = time.clock()
 runTimeQuery = endTimeQuery - startTimeQuery
